9e7273693addf1327c764bdee59d72059b97cde5
Steven Beales <stevenbeales@gmail.com>
2019-02-17 14:38:17 -0500

    add heroku

---
+++ heroku.zsh
@@ -0,0 +1,9 @@
+
+# heroku
+alias hrk='heroku'
+alias hp='git push heroku master'
+alias hl='heroku logs'
+alias ho='heroku open'
+alias hi='heroku info'
+alias hc='heroku console'
+alias hps='heroku ps'
